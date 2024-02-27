82a7b8d5fcaebc963c77ca92df5e9bcf833818fb
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-02-27 06:44:18 +0545

    added contact form,btn in contact.js

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -4,7 +4,10 @@ import Walmart from '../../assets/walmart.png'
 import Adobe from '../../assets/adobe.png'
 import Microsoft from '../../assets/microsoft.png'
 import Facebook from '../../assets/facebook.png'
-
+import FacebookIcon from '../../assets/facebook.png'
+import TwitterIcon from '../../assets/twitter.png'
+import YoutubeIcon from '../../assets/youtube.png'
+import InstagramIcon from '../../assets/instagram.png'
 const Contact = () => {
   return (
     <div>
@@ -22,6 +25,22 @@ const Contact = () => {
             </div>
         </div>
         <div className="contact"></div>
+        <h className="contactPageTitle">Contact Me</h>
+        <span className="contactDesc">Please fill out the form below to discuss any work opportunities</span>
+        <form className='contactForm'>
+          <input type="text" className="name" placeholder='Your Name' />
+          <input type="email" className="email" placeholder='Your email' />
+          <textarea name="message"  rows="5" placeholder='Your Message' className='msg'></textarea>
+          <button  type='submit' value='send'
+          className="submitBtn">Submit</button>
+          <div className="links">
+            <img src={FacebookIcon} alt="Facebook" className="link" />
+            <img src={TwitterIcon} alt="Twitter" className="link" />
+            <img src={YoutubeIcon} alt="Youtube" className="link" />
+            <img src={InstagramIcon} alt="Instagram" className="link" />
+          </div>
+
+        </form>
       </section>
     </div>
   )
