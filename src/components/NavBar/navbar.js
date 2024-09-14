e09202e1cf3516d82f185f365ea85921a9bbb428
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-09-14 08:53:50 +0545

    "Updated Contact page CSS and JS, replaced client logos with tech stack icons, and refactored Navbar component"

--- src/components/NavBar/navbar.js
+++ src/components/NavBar/navbar.js
@@ -9,38 +9,135 @@ const Navbar = () => {
   const [showMenu,setShowMenu]=useState(false)
   return (
     <nav className="navbar">
-        <img src={logo} alt="Logo" className='logo' />
-        <div className="desktopMenu">
-          <Link  activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500}
-          className="desktopMenuListItem">Home</Link>
-          <Link  activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500} className="desktopMenuListItem">Clients</Link>
-          <Link  activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500}className="desktopMenuListItem">About</Link>
-          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500}className="desktopMenuListItem">Portfolio</Link>
-        </div>
-        <button className="desktopMenuBtn" onClick={()=>{
-          document.getElementById('contact').scrollIntoView({
-            behavior:'smooth'
+      <img src={logo} alt="Logo" className="logo" />
+      <div className="desktopMenu">
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Tech Stack
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="desktopMenuListItem"
+        >
+          Portfolio
+        </Link>
+      </div>
+      <button
+        className="desktopMenuBtn"
+        onClick={() => {
+          document.getElementById("contact").scrollIntoView({
+            behavior: "smooth",
           });
-        }}>
-            <img   src={contactImg} alt="contact" className="desktopMenuImg" />
-            Contact Me
-        </button>
+        }}
+      >
+        <img src={contactImg} alt="contact" className="desktopMenuImg" />
+        Contact Me
+      </button>
 
-        <img src={menu} alt="Menu" className='mobMenu' onClick={()=>setShowMenu(!showMenu)} />
-        <div className="navMenu" style={{display: showMenu? 'flex':'none'}} >
-          <Link  activeClass='active' to='intro' spy={true} smooth={true} offset={-100} duration={500}
-          className="ListItem" onClick={()=>setShowMenu(false)} >Home</Link>
-          <Link  activeClass='active' to='clients' spy={true} smooth={true} offset={-100} duration={500} className="ListItem" onClick={()=>setShowMenu(false)}>Clients</Link>
-          <Link  activeClass='active' to='skills' spy={true} smooth={true} offset={-100} duration={500}className="ListItem"onClick={()=>setShowMenu(false)}>About</Link>
-          <Link activeClass='active' to='works' spy={true} smooth={true} offset={-100} duration={500}className="ListItem"onClick={()=>setShowMenu(false)}>Portfolio</Link>
-          <Link activeClass='active' to='contact' spy={true} smooth={true} offset={-100} duration={500}className="ListItem"onClick={()=>setShowMenu(false)}>Contact</Link>
-        </div>
-
-
-
-
+      <img
+        src={menu}
+        alt="Menu"
+        className="mobMenu"
+        onClick={() => setShowMenu(!showMenu)}
+      />
+      <div className="navMenu" style={{ display: showMenu ? "flex" : "none" }}>
+        <Link
+          activeClass="active"
+          to="intro"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="ListItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Home
+        </Link>
+        <Link
+          activeClass="active"
+          to="clients"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="ListItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Tech Stack
+        </Link>
+        <Link
+          activeClass="active"
+          to="skills"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="ListItem"
+          onClick={() => setShowMenu(false)}
+        >
+          About
+        </Link>
+        <Link
+          activeClass="active"
+          to="works"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="ListItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Portfolio
+        </Link>
+        <Link
+          activeClass="active"
+          to="contact"
+          spy={true}
+          smooth={true}
+          offset={-100}
+          duration={500}
+          className="ListItem"
+          onClick={() => setShowMenu(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
-  )
+  );
 }
 
 export default Navbar
