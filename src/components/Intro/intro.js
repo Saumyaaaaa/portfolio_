4a5306c21fde9ea90501312344d343186e04f3fa
SAUMYA NEUPANE <somyaneupane@gmail.com>
2024-09-16 16:18:27 +0545

    "Refactored Intro component CSS and JS files, updating styles, layout, and content."

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,22 +1,34 @@
-import React from 'react'
-import './intro.css'
-import bg from '../../assets/image.png'
-import btnImg from '../../assets/hireme.png'
-import {Link} from 'react-scroll'
+import React from "react";
+import "./intro.css";
+import bg from "../../assets/image.png";
 
 const Intro = () => {
   return (
-   <section id="intro">
-    <div className="introContent">
+    <section id="intro">
+      <div className="introContent">
         <span className="hello">Hello,</span>
-        <span className="introText">I'm <span className="introName">Saumya</span><br/>Web Developer</span>
-        <p className="introPara">I am a skilled web developer with experience in creating  <br/>visually appealing and user friendly websites. </p>
-        <Link><button className="btn"><img src={btnImg} alt="hire me" className='btnImg'/>Hire Me</button></Link>
-        
-    </div>
-    <img src={bg} alt="Profile" className="bg" />
-   </section>
-  )
-}
+        <span className="introText">
+          I'm <span className="introName">Saumya</span>
+          <br />
+          Web Developer
+        </span>
+        <p className="introPara">
+         experience in creating visually
+          appealing, responsive, and user-friendly websites. My expertise lies
+          in leveraging modern technologies like React, TypeScript, and Node.js
+          to build efficient web applications. I have a passion for clean,
+          scalable code and a strong understanding of frontend design principles
+          to enhance the user experience. Whether it's building single-page
+          applications or managing complex backend systems, I focus on
+          delivering high-quality, reliable solutions that meet client and user
+          needs. Let's work together to bring your ideas to life!
+        </p>
+      </div>
+      <div className="bgWrapper">
+        <img src={bg} alt="Profile" className="bg" />
+      </div>
+    </section>
+  );
+};
 
-export default Intro
+export default Intro;
